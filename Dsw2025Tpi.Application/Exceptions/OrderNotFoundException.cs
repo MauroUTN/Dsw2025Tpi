1cc5463eb1e1f98c1c24f1d91733e3e68e56b6df
Josemaria Henriquez Colacciopppo <Josemaria.HenriquezColaccioppo@alu.frt.utn.edu.ar>
2025-06-22 22:12:11 -0300

    adicion seeder

---
+++ Dsw2025Tpi.Application/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace Dsw2025Tpi.Application.Exceptions;
+
+public class OrderNotFoundException : Exception
+{
+    public OrderNotFoundException(Guid orderId)
+        : base($"No se encontró la orden con ID: {orderId}")
+    {
+    }
+}
