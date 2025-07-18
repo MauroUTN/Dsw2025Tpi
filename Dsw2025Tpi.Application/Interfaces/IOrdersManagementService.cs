bf41b9b12e671cabc5c47bb22c6b7deec0cc27b8
JosemariaHenriquezC <Josemaria.HenriquezColaccioppo@alu.frt.utn.edu.ar>
2025-07-18 11:15:46 -0300

    Modificacion de interfaces

---
+++ Dsw2025Tpi.Application/Interfaces/IOrdersManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Interfaces
+{
+    internal interface IOrdersManagementService
+    {
+        Task<OrderModel.ResponseOrderModel?> GetOrderById(Guid id);
+        Task<IEnumerable<OrderModel.ResponseOrderModel>?> GetAllOrders();
+        Task<OrderModel.ResponseOrderModel> AddOrder(OrderModel.RequestOrderModel request);
+        Task<OrderModel.ResponseOrderModel> PutOrder(OrderModel.RequestOrderModel request);
+    }
+}
---
+++ Dsw2025Tpi.Application/Interfaces/IProductsManagementService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Interfaces
+{
+    internal interface IProductsManagementService
+    {
+        Task<ProductModel.ResponseProductModel?> GetProductById(Guid id);
+        Task<IEnumerable<ProductModel.ResponseProductModel>?> GetAllProducts();
+        Task<ProductModel.ResponseProductModel> AddProduct(ProductModel.RequestProductModel request);
+        Task<ProductModel.ResponseProductModel> UpdateProduct(ProductModel.RequestProductModel request);
+        Task<ProductModel.ResponseProductModel> UpdateProduct(Guid id, ProductModel.RequestProductModel request);
+        Task<ProductModel.ResponseProductModel> PatchProduct(Guid id);
+    }
+}
