c51c6d813b4fab3ceb3643361c2864b958cccbfa
JosemariaHenriquezC <Josemaria.HenriquezColaccioppo@alu.frt.utn.edu.ar>
2025-07-18 10:54:04 -0300

    Cambios en las excepciones

---
+++ Dsw2025Tpi.Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Tpi.Application.Exceptions
+{
+    internal class EntityNotFoundException : ApplicationException
+    {
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+    }
+
+}
